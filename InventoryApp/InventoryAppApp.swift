aa8f089028b9b074bc3888d0f7747ee9fd6b2674
Mark Norgren <mark@marknorgren.com>
2023-02-21 09:14:59 -0600

    Initial Commit

---
+++ InventoryApp/InventoryAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  InventoryAppApp.swift
+//  InventoryApp
+//
+//  Created by mark on 2/21/23.
+//
+
+import SwiftUI
+
+@main
+struct InventoryAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
